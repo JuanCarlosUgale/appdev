6fb4f9987af3dbc900a0eee5384659b463b94414
Christian Solis <2225155@slu.edu.ph>
2022-12-10 11:13:32 +0800

    add arithmetic.swift

---
+++ arithmetic.swift
@@ -0,0 +1,14 @@
+var product = 0
+
+print("Enter the first number: ")
+if let num1 = Int(readLine()!){
+    product += num1
+}
+
+print("Enter the second number: ")
+if let num2 = Int(readLine()!){
+    product *= num2
+}
+
+print("The product: \(product)")
+
